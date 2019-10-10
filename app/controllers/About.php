3c0b4f3c7eabd9422dbe4c66092129c2059099c2
adnan hadiyana <adnanhadiyana6@mail.unpas.ac.id>
2019-10-10 10:00:15 +0700

    membuat contorllers

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About{
+    public function index($nama ='Adnan', $pekerjaan='Mahasiswa'){
+        echo 'Hallo Nama saya $nama, saya adalah seorang $pekerjaan ';
+    }
+    public function page(){
+        echo ' About/page';
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,39 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct(){
-        var_dump($_GET);
-        var_dump($url);
+        $url = $this->parseURL();
+
+        if(file_exists('../app/controllers/' . $url[0] . 'php')){
+            $this->controller =$url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+
+        if( isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method=$url[1];
+                unsert($url[1]);
+            }
+        }
+
+
+
+        if( !empty($url)){
+           $this->params = array_values($url);
+        }
+
+
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    
     }
 
     public function parseURL(){
